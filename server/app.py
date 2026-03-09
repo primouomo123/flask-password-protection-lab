a75f457dcb9a3f32d7c2275e8c59257378413585
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-03-09 19:59:28 -0400

    Signup route created

--- server/app.py
+++ server/app.py
@@ -15,7 +15,29 @@ class ClearSession(Resource):
 
         return {}, 204
 
+
+
+class Signup(Resource):
+
+    def post(self):
+
+        data = request.get_json()
+
+        new_user = User(username=data['username'])
+        new_user.password_hash = data['password']
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        user_schema = UserSchema()
+        user_dict = user_schema.dump(new_user)
+
+        session['user_id'] = new_user.id
+
+        return user_dict, 201
+
 api.add_resource(ClearSession, '/clear', endpoint='clear')
+api.add_resource(Signup, '/signup', endpoint='signup')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
